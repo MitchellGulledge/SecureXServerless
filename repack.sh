8a66246d0db5d835fcd1cf4edba251dee7e53d2b
joshand@cisco.com <joshand@cisco.com>
2020-10-29 22:31:15 -0500

    revert change

--- repack.sh
+++ repack.sh
@@ -1,3 +1,4 @@
 rm MerakiSecureXAutomation/*.log
 rm *.zip
-zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x 'requirements.txt'
+#zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x 'requirements.txt'
+zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x '.python_packages/*'
