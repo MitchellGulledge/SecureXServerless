38dd21fd580fc66ecea67bd3d498ac6f1a83db27
joshand@cisco.com <joshand@cisco.com>
2020-11-01 11:30:14 -0600

    update repack script

--- repack.sh
+++ repack.sh
@@ -2,3 +2,5 @@ rm MerakiSecureXAutomation/*.log
 rm *.zip
 #zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x 'requirements.txt'
 zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x '.python_packages/*'
+git add MerakiFunction.zip
+git commit -m "repack"
