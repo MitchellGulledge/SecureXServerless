3d87bfca87765b6dcb3c0f981a36a379734dc9fe
joshand@cisco.com <joshand@cisco.com>
2020-11-01 11:28:15 -0600

    update to built in function

--- MerakiSecureXAutomation/__init__.py
+++ MerakiSecureXAutomation/__init__.py
@@ -1,11 +1,7 @@
 import logging
 import azure.functions as func
-from azf_wsgi import AzureFunctionsWsgi
 from ..FlaskApp.app import app as application
 
 
-# def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-#     return func.WsgiMiddleware(application).handle(req, context)
-
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return AzureFunctionsWsgi(application).main(req, context)
+    return func.WsgiMiddleware(application).handle(req, context)
