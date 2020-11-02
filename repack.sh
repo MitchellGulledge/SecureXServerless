08e4117cf030258aec364793e8d230c082a3d35b
joshand@cisco.com <joshand@cisco.com>
2020-11-02 13:53:12 -0600

    update repack script to rebuild pip packages

--- repack.sh
+++ repack.sh
@@ -1,5 +1,7 @@
 rm MerakiSecureXAutomation/*.log
 rm *.zip
+rm -rf .python_packages
+pip install --target=./.python_packages/lib/site-packages -r requirements.txt
 zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x 'requirements.txt'
 #zip -r MerakiFunction.zip . -x 'venv/*' -x '.idea/*' -x '.git/*' -x '.python_packages/*'
 git add MerakiFunction.zip
