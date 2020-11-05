89917cd5f81cb7061430167ad2c9f814cb3e3ea1
joshand@cisco.com <joshand@cisco.com>
2020-11-05 12:08:54 -0600

    add respond observables items

--- FlaskApp/api/respond.py
+++ FlaskApp/api/respond.py
@@ -1,10 +1,11 @@
 from functools import partial
-from flask import Blueprint
+from flask import Blueprint, request
 from .schemas import ObservableSchema, ActionFormParamsSchema
 from .utils import get_json, get_jwt, jsonify_data
+from ..shared_code import meraki
+
 
 respond_api = Blueprint('respond', __name__)
-
 
 get_observables = partial(get_json, schema=ObservableSchema(many=True))
 get_action_form_params = partial(get_json, schema=ActionFormParamsSchema())
@@ -13,8 +14,36 @@ get_action_form_params = partial(get_json, schema=ActionFormParamsSchema())
 @respond_api.route('/respond/observables', methods=['POST'])
 def respond_observables():
     _ = get_jwt()
-    _ = get_observables()
-    return jsonify_data([])
+    meraki_config = meraki.MerakiConfig()
+    json_refer = []
+
+    try:
+        ob = get_observables()
+    except Exception:
+        return jsonify_data(json_refer)
+
+    if ob[0]["type"] == "ip":
+        cli = meraki_config.find_meraki_client(client_ip=ob[0]["value"])
+        if not cli:
+            json_refer = [
+                {
+                    "id": "meraki-firewall-block",
+                    "title": "Block this IP Address",
+                    "description": "Add Firewall rule to block this IP in Meraki Dashboard",
+                    "query-params": {}
+                }
+            ]
+        else:
+            json_refer = [
+                {
+                    "id": "meraki-client-block",
+                    "title": "Quarantine this Client",
+                    "description": "Assign 'Block' Group Policy to this Client.",
+                    "query-params": {}
+                }
+            ]
+
+    return jsonify_data(json_refer)
 
 
 @respond_api.route('/respond/trigger', methods=['POST'])
